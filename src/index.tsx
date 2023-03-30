5e08f713dea0ba648e922d8ecbb1654a0e228e19
Iago Campista <52842511+IagoCampista@users.noreply.github.com>
2023-03-29 23:20:31 -0300

    add basic router

--- src/index.tsx
+++ src/index.tsx
@@ -6,15 +6,19 @@ import reportWebVitals from './reportWebVitals';
 import ClientRegistration from './pages/ClientRegistration';
 import api_axios from './api'
 import Catalogo from './pages/Catalog';
+import {BrowserRouter, Route, Routes } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    {/* <ClientRegistration api = {api_axios}/> */}
-    <Catalogo api = {api_axios}/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ClientRegistration api = {api_axios}/>} />
+        <Route path="/catalogo" element={<Catalogo api = {api_axios}/>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
